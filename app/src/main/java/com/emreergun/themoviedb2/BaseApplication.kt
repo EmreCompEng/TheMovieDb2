8bbba08b9153cc62830a9d223df3b035448cc900
Emre <emreergun@bgnmobi.com>
2021-05-26 10:28:48 +0300

    Added Base Application for Hilt dependency injection

---
+++ app/src/main/java/com/emreergun/themoviedb2/BaseApplication.kt
@@ -0,0 +1,9 @@
+package com.emreergun.themoviedb2
+
+import dagger.hilt.android.HiltAndroidApp
+
+// Hilt Android App Seviyesinde Başlatılır
+// En Zirve Yerdir => Singleton
+@HiltAndroidApp
+class BaseApplication {
+}
---
+++ app/src/main/java/com/emreergun/themoviedb2/ui/main/populermovies/PopulerMoviesViewModel.kt
@@ -0,0 +1,6 @@
+package com.emreergun.themoviedb2.ui.main.populermovies
+
+import androidx.lifecycle.ViewModel
+
+class PopulerMoviesViewModel : ViewModel() {
+}
