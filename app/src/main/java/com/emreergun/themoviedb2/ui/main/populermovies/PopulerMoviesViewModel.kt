f56c3df8124f23612752a277af1441c6fa17e798
Emre <emreergun@bgnmobi.com>
2021-05-27 12:35:59 +0300

    Controlled populer movie page

--- app/src/main/java/com/emreergun/themoviedb2/ui/main/populermovies/PopulerMoviesViewModel.kt
+++ app/src/main/java/com/emreergun/themoviedb2/ui/main/populermovies/PopulerMoviesViewModel.kt
@@ -26,37 +26,39 @@ class PopulerMoviesViewModel @Inject constructor(
 
 
     // Fetch data in repostiory with retrofit http
-    fun observePopulerMovies(){
-        zipObservable()
+    fun observePopulerMovies() {
+        if (currentPage <= 997)
+            zipObservable()
     }
 
     // Sıralı Birleştirme
-    private var currentPage=1
+    private var currentPage = 1 // Max Page 1000
     private fun zipObservable() {
-        _moviesLiveData.value=MovieResource.Loading()
+        _moviesLiveData.value = MovieResource.Loading()
         Observable
             .merge(
                 repository.getPopulerMovies(currentPage),
-                repository.getPopulerMovies(currentPage+1),
-                repository.getPopulerMovies(currentPage+2),
-                repository.getPopulerMovies(currentPage+3),
+                repository.getPopulerMovies(currentPage + 1),
+                repository.getPopulerMovies(currentPage + 2),
+                repository.getPopulerMovies(currentPage + 3),
             )
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeOn(Schedulers.io())
-            .subscribe(object :DisposableObserver<PopulerMovies>(){
+            .subscribe(object : DisposableObserver<PopulerMovies>() {
                 override fun onNext(populerMovies: PopulerMovies) {
                     Log.d(TAG, "onNext: Current Page $currentPage")
-                    _moviesLiveData.value=MovieResource.Success(populerMovies)  // Successs....
+                    _moviesLiveData.value = MovieResource.Success(populerMovies)  // Successs....
                 }
 
                 override fun onError(error: Throwable) {
-                    _moviesLiveData.value=MovieResource.Error("error :${error.message}") // Error.....
+                    _moviesLiveData.value =
+                        MovieResource.Error("error :${error.message}") // Error.....
                 }
 
                 override fun onComplete() {
                     Log.d(TAG, "onComplete: ")
                     Log.d(TAG, "onComplete: Last Page :$currentPage")
-                    currentPage+=4
+                    currentPage += 4
                 }
 
             })
